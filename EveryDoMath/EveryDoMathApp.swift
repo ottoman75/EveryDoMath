63dcf95d6c7c481c750b10996ab1df8006e52e11
officer75 <officer75@nate.com>
2026-04-08 19:55:02 +0900

    Initial Commit

---
+++ EveryDoMath/EveryDoMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryDoMathApp.swift
+//  EveryDoMath
+//
+//  Created by otto on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct EveryDoMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EveryDoMathTests/EveryDoMathTests.swift
@@ -0,0 +1,19 @@
+//
+//  EveryDoMathTests.swift
+//  EveryDoMathTests
+//
+//  Created by otto on 4/8/26.
+//
+
+import Testing
+@testable import EveryDoMath
+
+struct EveryDoMathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
